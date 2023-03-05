197a8d6220a3e71e1c9e3f505f19f8393454d8ff
Shubham <111031742+kshubham4141@users.noreply.github.com>
2023-03-05 10:29:09 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Git
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        //Addition 
+        System.out.print("Enter First Number: ");
+        int a=s.nextInt();
+        System.out.print("Enter Second Number: ");
+        int b=s.nextInt();
+        int c=a+b;
+        System.out.print("Addition: "+c);
+
+    }
+}
